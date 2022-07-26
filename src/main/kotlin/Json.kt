a09fe7786dee42a113c719d574600f897a37cb23
who-is-hu <dydgndlaek97@gmail.com>
2022-07-26 23:36:26 +0900

    day2 - StringBuilder의 확장함수로 옮기면서 중복 제거

--- src/main/kotlin/Json.kt
+++ src/main/kotlin/Json.kt
@@ -3,47 +3,47 @@ import kotlin.reflect.KProperty
 class Json0(val a: Int, val b: String)
 
 fun main(args: Array<String>) {
-    println(stringify(Json0(3, "abc")))
+    val stringBuilder = StringBuilder()
+    val jsonString = stringBuilder.stringify(Json0(3, "abc"))
+    println(jsonString)
 }
 
-fun stringify(value: Any): String {
-    val builder = StringBuilder()
-    jsonValue(value, builder)
-    return builder.toString()
+fun StringBuilder.stringify(value: Any): String {
+    jsonValue(value)
+    return toString()
 }
 
-private fun jsonValue(value: Any?, builder: StringBuilder) {
+private fun StringBuilder.jsonValue(value: Any?) {
     when (value) {
-        null -> builder.append("null") // 여길 타면 아래는 null 이 아니게 smart casting
-        is String -> jsonString(value, builder)
-        is Number, is Boolean -> builder.append("$value")
-        is List<*> -> jsonList(value, builder)
-        else -> jsonObject(value, builder)
+        null -> append("null") // 여길 타면 아래는 null 이 아니게 smart casting
+        is String -> jsonString(value)
+        is Number, is Boolean -> append("$value")
+        is List<*> -> jsonList(value)
+        else -> jsonObject(value)
     }
 }
 
-private fun jsonString(v: String, builder: StringBuilder) {
-    builder.append( """"${v.replace("\"", "\\\"")}"""")
+private fun StringBuilder.jsonString(v: String) {
+    append( """"${v.replace("\"", "\\\"")}"""")
 }
 
-private fun jsonList(target: List<*>, builder: StringBuilder) {
-    builder.append("[")
-    target.joinTo({builder.append(",")}) { jsonValue(it,builder)}
-    builder.append("]")
+private fun StringBuilder.jsonList(target: List<*>) {
+    append("[")
+    target.joinTo({append(",")}) { jsonValue(it) }
+    append("]")
 }
 
-private fun <T : Any> jsonObject(target: T, builder: StringBuilder): String {
-    builder.append("{")
+private fun <T : Any> StringBuilder.jsonObject(target: T) {
+    append("{")
     target::class.members.filterIsInstance<KProperty<*>>()
-        .joinTo({ builder.append(",") }) {
+        .joinTo({ append(",") }) {
             //여기서 KProperty로 변환되서 옴
             //property는 getter를 가지고있고 var이면 setter도
-            builder.append("${jsonValue(it.name, builder)}")
-            builder.append(":")
-            builder.append("${jsonValue(it.getter.call(target), builder)}")
-        }.toString()
-    builder.append("}")
-    return builder.toString()
+            jsonValue(it.name)
+            append(":")
+            jsonValue(it.getter.call(target))
+        }
+    append("}")
 }
 
 fun <T> Iterable<T>.joinTo(sep: () -> Unit, transform: (T) -> Unit) {
