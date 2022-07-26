55b0f483121e2a210a083bdf0fef4a9eefa9eaaf
who-is-hu <dydgndlaek97@gmail.com>
2022-07-27 02:40:21 +0900

    day2 - wrap 메서드 써서 중복 또 줄임+ 수신객체지정 람다 여러 테스트

--- src/main/kotlin/Json.kt
+++ src/main/kotlin/Json.kt
@@ -13,6 +13,46 @@ fun StringBuilder.stringify(value: Any): String {
     return toString()
 }
 
+fun StringBuilder.comma() {
+    append(",")
+}
+
+// 수신 객체 지정 람다는 Function1으로 바뀜
+// Function1::invoke(StringBuilder)
+fun StringBuilder.wrap(begin: Char, end: Char, block: StringBuilder.() -> Unit) {
+    append(begin)
+    block() // == this.block() == block(this)
+    append(end)
+}
+
+// Function0 으로 바뀜
+// Function0::invoke()
+fun StringBuilder.wrap2(begin: Char, end: Char, block: () -> Unit) {
+    append(begin)
+    block()
+    // this.block() invoke가 받는게 없어서 this 사용불가
+    append(end)
+}
+
+// 람다 본문 this 사용 불가
+//fun test(){
+//    val stringBuilder = StringBuilder()
+//    stringBuilder.wrap2('a','a') {
+//        println(this)
+//    }
+//}
+
+// 람다 밖에서 this 를 찾을수 있을때 사용가능
+class Test(val prop: String) {
+    fun testMethod(): Unit {
+        val stringBuilder = StringBuilder()
+        stringBuilder.wrap2('a','a') {
+            println(this.prop)
+        }
+    }
+}
+
+// 전역 레벨 private 은 file 안에서
 private fun StringBuilder.jsonValue(value: Any?) {
     when (value) {
         null -> append("null") // 여길 타면 아래는 null 이 아니게 smart casting
@@ -24,26 +64,27 @@ private fun StringBuilder.jsonValue(value: Any?) {
 }
 
 private fun StringBuilder.jsonString(v: String) {
-    append( """"${v.replace("\"", "\\\"")}"""")
+    append(""""${v.replace("\"", "\\\"")}"""")
 }
 
 private fun StringBuilder.jsonList(target: List<*>) {
-    append("[")
-    target.joinTo({append(",")}) { jsonValue(it) }
-    append("]")
+    wrap('[', ']') {
+        target.joinTo(::comma) { jsonValue(it) }
+    }
 }
 
+
 private fun <T : Any> StringBuilder.jsonObject(target: T) {
-    append("{")
-    target::class.members.filterIsInstance<KProperty<*>>()
-        .joinTo({ append(",") }) {
-            //여기서 KProperty로 변환되서 옴
-            //property는 getter를 가지고있고 var이면 setter도
-            jsonValue(it.name)
-            append(":")
-            jsonValue(it.getter.call(target))
-        }
-    append("}")
+    wrap('{', '}') {
+        target::class.members.filterIsInstance<KProperty<*>>()
+            .joinTo(::comma) { // this에서 찾고 없으면 전역에서 찾음
+                //여기서 KProperty로 변환되서 옴
+                //property는 getter를 가지고있고 var이면 setter도
+                jsonValue(it.name)
+                append(":")
+                jsonValue(it.getter.call(target))
+            }
+    }
 }
 
 fun <T> Iterable<T>.joinTo(sep: () -> Unit, transform: (T) -> Unit) {
